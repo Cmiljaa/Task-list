412a13020b98a3e38ec34c9bce33248070c92cd3
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-07-08 02:26:03 +0200

    Get tasks from the database

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,10 @@
 @section('content')
 <div>
     @forelse ($tasks as $task)
-        <a href="{{route('tasks.show', ['id => $task -> id'])}}">
-            <p>{{$task -> title}}</p>
-        </a>
+    <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+        <p>{{ $task->title }}</p>
+        
+    </a>
     @empty
         <p>There are no tasks</p>
     @endforelse
